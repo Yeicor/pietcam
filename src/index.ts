ca3ecc92d3aba1aff734f4e0800e07abc51a4ee9
Yeicor <4929005+Yeicor@users.noreply.github.com>
2023-05-14 22:10:03 +0200

    Restart once more

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,2 @@
-import { createApp } from 'vue'
-import App from './app.vue'
-import store from './store'
-import { createVuetify } from 'vuetify'
-import 'vuetify/lib/styles/main.css'
-import * as components from 'vuetify/lib/components'
-import * as directives from 'vuetify/lib/directives'
-
-import router from './router'
-
-// Use node emulated globals from Parcel (https://parceljs.org/features/node-emulation/) to set Vue options
-if (process.env.NODE_ENV === 'development') {
-  console.log('Development mode')
-  globalThis.__VUE_OPTIONS_API__ = true
-  globalThis.__VUE_PROD_DEVTOOLS__ = true
-} else {
-  globalThis.__VUE_OPTIONS_API__ = false
-  globalThis.__VUE_PROD_DEVTOOLS__ = false
+export function to_do() {
 }
-
-const app = createApp(App)
-app.use(store)
-app.use(router)
-app.use(createVuetify({ components, directives }))
-app.mount('#app')
--- src/types.d.ts
+++
@@ -1,8 +0,0 @@
-declare let process: { env: { NODE_ENV: string } }
-declare module '*.vue';
-declare module 'vue-router' {
-  interface RouteMeta {
-    title: string
-    icon: string
-  }
-}
